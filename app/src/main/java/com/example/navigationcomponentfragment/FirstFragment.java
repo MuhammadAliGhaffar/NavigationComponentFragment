a9a34d8f497b732e3ec42ce65597ad3d8f0808e6
MuhammadAliGhaffar <alibrohi999@gmail.com>
2021-02-07 20:15:54 +0500

    navigation component

---
+++ app/src/main/java/com/example/navigationcomponentfragment/FirstFragment.java
@@ -0,0 +1,31 @@
+package com.example.navigationcomponentfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class FirstFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        view.findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getView()).navigate(R.id.action_firstFragment_to_secondFragment);
+            }
+        });
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponentfragment/SecondFragment.java
@@ -0,0 +1,32 @@
+package com.example.navigationcomponentfragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+
+public class SecondFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_second, container, false);
+        view.findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getView()).navigate(R.id.action_secondFragment_to_firstFragment);
+            }
+        });
+        return view;
+    }
+}
